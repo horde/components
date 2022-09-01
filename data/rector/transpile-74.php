2178282be226bcc9ed45b65a93ebc6dbffe747ce
Ralf Lang <ralf.lang@ralf-lang.de>
2022-09-01 20:24:53 +0200

    Add prototype of a transpiler pipeline

---
+++ data/rector/transpile-74.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/test',        
+    ]);
+
+    $rectorConfig->sets([
+        DowngradeLevelSetList::DOWN_TO_PHP_74
+    ]);
+};
