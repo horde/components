6358a574ebba4388ddafffab94d37a64e354ee42
Ralf Lang <ralf.lang@ralf-lang.de>
2022-11-12 21:28:42 +0100

    Add web hook

---
+++ ci.php
@@ -0,0 +1,3 @@
+<?php
+require_once dirname(__FILE__, 3) . '/vendor/autoload.php';
+Horde\Components\WebBootstrap::run();
---
+++ src/WebBootstrap.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Horde\Core;
+
+use Horde\Core\Middleware\AppFinder;
+use Horde\Core\Middleware\AppRouter;
+use Horde\Http\RequestFactory;
+use Horde\Http\UriFactory;
+use Horde\Http\ResponseFactory;
+use Horde\Http\Server\RampageRequestHandler;
+use Horde\Http\Server\RequestBuilder;
+use Horde\Http\Server\ResponseWriterWeb;
+use Horde\Http\Server\Runner;
+use Horde\Http\Server\Middleware\Responder;
+use Horde\Http\StreamFactory;
+use Horde\Core\Middleware\HordeCore as HordeCoreMiddleware;
+use Horde\Injector\Injector;
+use Horde\Injector\TopLevel;
+
+/**
+ * Bootstrap the Components HTTP endpoint
+ *
+ *
+ */
+class WebBootstrap
+{
+    public static function run()
+    {
+        $requestFactory = new RequestFactory();
+        $streamFactory = new StreamFactory();
+        $uriFactory = new UriFactory();
+        $responseFactory = new ResponseFactory();
+
+        // Build the request from server variables.
+        // The RequestBuilder could easily be autowired by a DI container.
+        $requestBuilder = new RequestBuilder($requestFactory, $streamFactory, $uriFactory);
+        $request = $requestBuilder->withGlobalVariables()->build();
+        $injector = new Horde\Injector\TopLevel();
+
+        $handler = new RampageRequestHandler($responseFactory, $streamFactory, $middlewares);
+        $runner = new Runner($handler, new ResponseWriterWeb());
+        $runner->run($request);
+    }
+}
