cd8d80209502a870b80a9807b1ab84ce379c9638
Ralf Lang <ralf.lang@ralf-lang.de>
2023-12-27 08:33:29 +0100

    ComponentDirectory - decouple directory stuff from higher level logic

---
+++ src/Component/ComponentDirectory.php
@@ -0,0 +1,34 @@
+<?php
+namespace Horde\Components\Component;
+use Horde\Components\Component;
+use Horde\Components\Component\DependencyList;
+use Horde\Components\Helper\Commit as HelperCommit;
+use Horde\Components\Helper\Root as HelperRoot;
+use Horde\Components\Pear\Environment as PearEnvironment;
+use Horde\Components\RuntimeContext\CurrentWorkingDirectory;
+use stdClass;
+
+class ComponentDirectory
+{
+    private string $fullPath;
+
+    public function __construct(CurrentWorkingDirectory|string $dir)
+    {
+        $this->fullPath = (string) $dir;
+    }
+
+    public function hasHordeYml(): bool
+    {
+        return is_file($this->fullPath . '/.horde.yml');
+    }
+
+    public function hasPackageXml(): bool
+    {
+        return is_file($this->fullPath . '/package.xml');
+    }
+
+    public function hasComposerJson(): bool
+    {
+        return is_file($this->fullPath . '/composer.json');
+    }
+}
