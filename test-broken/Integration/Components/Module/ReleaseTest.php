c8a9c349fe6c8b7a9030b19131fffad190a581aa
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-02 19:47:14 +0100

    style: php-cs-fixer removes outdated 'PHP Version 7' comments

--- test-broken/Integration/Components/Module/ReleaseTest.php
+++ test-broken/Integration/Components/Module/ReleaseTest.php
@@ -3,8 +3,6 @@
 /**
  * Test the Release module.
  *
- * PHP Version 7
- *
  * @category   Horde
  * @package    Components
  * @subpackage UnitTests
@@ -19,7 +17,7 @@ use Horde\Components\StoryTestCase;
 /**
  * Test the Release module.
  *
- * Copyright 2010-2024 Horde LLC (http://www.horde.org/)
+ * Copyright 2010-2026 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
